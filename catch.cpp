e9c4e9bfee04e0d4afb412736f7b73afb1d81503
MarkBrubaker <mbrubaker@smu.edu>
2020-02-06 09:00:28 -0600

    Added tests

---
+++ catch.cpp
@@ -0,0 +1,70 @@
+#include "catch.hpp"
+
+#include <cstring>
+#include "strand.hpp"
+
+TEST_CASE("String class", "[string]"){
+    strand s[10];
+    s[0] = strand("testString");
+    s[1] = strand("a test string");
+    s[2] = strand("");
+    s[3] = strand("THIS IS AN UPPERCASE STRING");
+    s[4] = strand("this is an uppercase string");
+    s[5] = strand("\n");
+    s[6] = strand("");
+    s[7] = strand("  split  split  split  ");
+    s[8] = strand("                          ");
+    s[9] = strand("testString");
+
+    SECTION("Equality operators"){
+        REQUIRE(s[0] == strand("testString"));
+        REQUIRE(s[0] == s[9]);
+        REQUIRE(s[2] == "");
+        REQUIRE(s[1] == "a test string");
+        REQUIRE(!(s[3] == s[4]));
+    }
+    SECTION("Assignment operators"){
+        strand str;
+        str = "a test string";
+        REQUIRE(str == s[1]);
+        str = strand("testString");
+        REQUIRE(str == s[0]);
+        str = "";
+        REQUIRE(str == s[6]);
+        str = strand("\n");
+        REQUIRE(str == s[5]);
+    }
+    SECTION("Addition operator"){
+        REQUIRE(strand("testStringtestString") == s[0]+s[9]);
+        REQUIRE(s[6] + s[6] == "");
+        REQUIRE(s[5] + s[6] == strand("\n"));
+        REQUIRE(s[0] + s[1] + s[2] == "testStringa test string");
+    }
+    /*SECTION("Greater than operator"){
+        REQUIRE(s[0] > s[1]);
+        REQUIRE(s[4] > s[3]);
+        REQUIRE(s[9] > s[6]);
+        REQUIRE(s[7] > s[6]);
+    }*/
+    SECTION("[] Operator"){
+        REQUIRE(s[0][1] == 'e');
+        REQUIRE(s[4][4] == ' ');
+        REQUIRE(s[6][0] == 0);
+    }
+    SECTION("getLength function"){
+        REQUIRE(s[9].size() == 10);
+        REQUIRE(s[2].size() == 0);
+        REQUIRE(s[8].size() == 26);
+        REQUIRE(s[3].size() == 27);
+    }
+    SECTION("Substring function"){
+        REQUIRE(s[0].substr(0, 5) == "testS");
+        REQUIRE(s[4].substr(0, 4) == "this");
+        REQUIRE(s[4].substr(1, 3) == "his");
+    }
+    SECTION("c_str function"){
+        REQUIRE(strcmp(s[0].c_str(), "testString") == 0);
+        REQUIRE(strcmp(s[9].c_str(), s[0].c_str()) == 0);
+        REQUIRE(strcmp(s[2].c_str(), "") == 0);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
 #include <iostream>
 #include "strand.hpp"
 #include "analyzer.hpp"
 
+int runTests() {
+    return Catch::Session().run();
+}
+
 int main(int argc, char **argv) {
+    if(argc < 2) return runTests();
+
     std::ios_base::sync_with_stdio(false);
     std::ifstream data(argv[1]);
     std::ifstream target(argv[2]);

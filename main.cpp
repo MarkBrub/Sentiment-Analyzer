f0cf3bec5802ba272b3abf0aea356ba2dbe2e7a7
MarkBrubaker <mbrubaker@smu.edu>
2020-02-07 10:12:26 -0600

    outputs words and frequencies

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-//#define CATCH_CONFIG_RUNNER
-//#include "catch.hpp"
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
 
 #include <iostream>
 #include "strand.hpp"
 #include "analyzer.hpp"
 
 int runTests() {
-    //return Catch::Session().run();
+    return Catch::Session().run();
     return 0;
 }
 
 int main(int argc, char **argv) {
-    //if(argc < 2) return runTests();
+    if(argc < 2) return runTests();
 
     std::ios_base::sync_with_stdio(false);
     std::ifstream trainData(argv[3]);
